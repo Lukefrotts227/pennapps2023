b94808ff8747cf1d2f34f1435e3187c632e5b347
Ljf227 <ljfrotton@gmail.com>
2023-09-09 17:21:57 -0400

    outlines

---
+++ pennapps/client/src/components/Minibar.js
@@ -0,0 +1,15 @@
+import React from 'react'; 
+
+
+const Minibar = () => {
+
+
+    return(
+        <div> 
+
+        </div> 
+
+    ); 
+}
+
+export default Minibar; 
---
+++ pennapps/client/src/components/Post.js
@@ -0,0 +1,16 @@
+import React from 'react'; 
+
+
+function Post(){
+
+
+    return(
+        <div> 
+
+        </div> 
+
+    );
+}
+
+
+export default Post; 
