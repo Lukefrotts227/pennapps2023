d69ddd10759ba0abdf82095b991f430d36cc76fb
Ljf227 <ljfrotton@gmail.com>
2023-09-08 22:14:29 -0400

    no slay for you

--- main.py
+++ main.py
@@ -1,14 +1,34 @@
 from flask import Flask
+from flask_mongoengine import MongoEngine
 
 
-nothing = False
+# Lfrotts DXYpIbztQfUBk2UH
+# koolkusum OpwS9i5kEXm7imJV
 
+#  mongodb+srv://<username>:<password>@cluster0.x0pvfba.mongodb.net/?retryWrites=true&w=majority
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
     return 'Hello, World!'
+
+
+
+app.config['MONGODB_SETTINGS'] = {
+    'db': 'your_database',
+    'host': 'localhost',
+    'port': 27017
+}
+db = MongoEngine()
+db.init_app(app)
+
+
+
+class User(db.Document):
+    name = db.StringField()
+    email = db.StringField()
+
 
 if __name__ == 'main':
     app.run()
