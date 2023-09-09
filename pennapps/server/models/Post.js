9d3942d8709b63b98561f02b88b56bff83b4876f
Ljf227 <ljfrotton@gmail.com>
2023-09-09 13:20:32 -0400

    check it

--- pennapps/server/models/Post.js
+++ pennapps/server/models/Post.js
@@ -2,15 +2,17 @@ const mongoose = require('mongoose');
 
 const PostSchema = new mongoose.Schema({
     title : {
-
+        type : String
     }, 
     capiton : {
-
+        type : String
     }, 
     image : {
-
+        data: Buffer, // Store the image data as binary
+        contentType: String, 
     }, 
     user : {
+        type : String
 
     }
 
