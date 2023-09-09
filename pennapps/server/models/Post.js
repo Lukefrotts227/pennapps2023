9f22a75046339b90cc865db95d74305230addc23
Ljf227 <ljfrotton@gmail.com>
2023-09-09 12:26:21 -0400

    updating prev

---
+++ pennapps/server/models/Post.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose'); 
+
+const PostSchema = new mongoose.Schema({
+    title : {
+
+    }, 
+    capiton : {
+
+    }, 
+    image : {
+
+    }
+
+}); 
--- pennapps/server/models/Prompts.js
+++ pennapps/server/models/Prompts.js
@@ -1,2 +1,12 @@
 const mongoose = require('mongoose'); 
 
+const PromptSchema = new mongoose.Schema({
+    title: {
+        type : string, 
+        required : string, 
+        minLength : 1, 
+        maxLenght : 40
+    }
+
+
+}); 
