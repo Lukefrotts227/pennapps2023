538ff20284b52c1a6f1f2542d3690b4595fe056c
Ljf227 <ljfrotton@gmail.com>
2023-09-09 18:16:33 -0400

    my commit

--- pennapps/client/src/components/Post.js
+++ pennapps/client/src/components/Post.js
@@ -3,26 +3,17 @@ import React from 'react';
 
 function Post(){
 
-    const handleSubmit = () => {
-
-
-    }
 
     return(
         <div> 
-        <form onSubmit = {handleSubmit}> 
-        
-        <input
-        id = "title"
-        name = "title"
-        />
+            <form> 
 
-        </form> 
+            </form> 
+
 
         </div> 
 
     );
 }
-
 
 export default Post; 
