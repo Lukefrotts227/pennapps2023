1908a3451509ba2d06184e22ebbb4e6f72c0b799
Ljf227 <ljfrotton@gmail.com>
2023-09-09 17:26:59 -0400

    my other commit no slayiing was done

---
+++ pennapps/client/src/components/TemplatePost.js
@@ -0,0 +1,17 @@
+import React from 'react'; 
+
+
+const TemplatePost = () => {
+
+
+    return(
+        <div>
+            
+        </div>
+       
+        );
+
+}
+
+export default TemplatePost; 
+
