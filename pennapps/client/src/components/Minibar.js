7592742983bd3778a3621fc6ab5fa7c2691afc36
Ljf227 <ljfrotton@gmail.com>
2023-09-09 17:25:02 -0400

    it

--- pennapps/client/src/components/Minibar.js
+++ pennapps/client/src/components/Minibar.js
@@ -4,12 +4,14 @@ import React from 'react';
 const Minibar = () => {
 
 
-    return(
-        <div> 
-
-        </div> 
-
-    ); 
+    return (
+        <nav>
+          <ul>
+            <li><Link to="/Home">Home</Link></li>
+            <li><Link to="/Post">About</Link></li>
+          </ul>
+        </nav>
+      );
 }
 
 export default Minibar; 
